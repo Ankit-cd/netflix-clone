848a85d6aebf5cfb5c046bcb641af13370bdeebf
Ankit_cd <ankitmehta4526@gmail.com>
2024-11-16 18:40:05 +0530

    tv and movie routes added

--- backend/config/envVars.js
+++ backend/config/envVars.js
@@ -1,9 +1,11 @@
 import dotenv from "dotenv";
 
 dotenv.config();
+
 export const ENV_VARS = {
     MONGO_URL:process.env.DATABASE_URL,
     PORT:process.env.PORT || 4000,
     JWT_SECRET:process.env.JWT_SECRET,
-    NODE_ENV:process.env.NODE_ENV
+    NODE_ENV:process.env.NODE_ENV,
+    TMDB_API_KEY:process.env.TMDB_API_KEY,
 }
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import { ENV_VARS } from './config/envVars.js';
 
 import authRoute from './routes/authRoute.js';
+import movieRoutes from './routes/movieRoutes.js';
+import tvRoutes from './routes/tvRoutes.js';
 import { connectDB } from './config/dB.js';
 
 dotenv.config();
@@ -13,11 +15,16 @@ const PORT = ENV_VARS.PORT;
 app.use(express.json())
 
 app.use('/api/v1/auth',authRoute);
+app.use('/api/v1/movie',movieRoutes);
+app.use('/api/v1/tv',tvRoutes);
 
 app.listen(PORT,()=>{
     console.log(`server started at http://localhost:${PORT}`);
     connectDB();
-})
+});
+
+
+
 
 
 
