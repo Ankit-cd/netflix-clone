27b09024d255f47fd8567a235fb5679425b4142d
Ankit_cd <ankitmehta4526@gmail.com>
2024-11-18 11:14:51 +0530

    Search routes and middleware added

--- backend/server.js
+++ backend/server.js
@@ -5,18 +5,23 @@ import { ENV_VARS } from './config/envVars.js';
 import authRoute from './routes/authRoute.js';
 import movieRoutes from './routes/movieRoutes.js';
 import tvRoutes from './routes/tvRoutes.js';
+import searchRoutes from './routes/searchRoutes.js';
 import { connectDB } from './config/dB.js';
+import { protectRoute } from './middleware/protectRoute.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 const app = express();
 
 const PORT = ENV_VARS.PORT;
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser())
 
 app.use('/api/v1/auth',authRoute);
-app.use('/api/v1/movie',movieRoutes);
-app.use('/api/v1/tv',tvRoutes);
+app.use('/api/v1/movie',protectRoute,movieRoutes);
+app.use('/api/v1/tv',protectRoute,tvRoutes);
+app.use('/api/v1/search',protectRoute,searchRoutes);
 
 app.listen(PORT,()=>{
     console.log(`server started at http://localhost:${PORT}`);
