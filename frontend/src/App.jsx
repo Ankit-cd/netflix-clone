06307f3949a4d0a43d20333e50fd1d9760d8f161
Ankit_cd <ankitmehta4526@gmail.com>
2024-11-28 22:07:54 +0530

    Watch page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import { Toaster } from "react-hot-toast";
 import { useAuthStore } from "./store/authUser";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
+import WatchPage from "./pages/WatchPage";
 
 
 function App() {
@@ -31,7 +32,9 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/login" element={!user ?<LoginPage/> : <Navigate to={"/"}/>}/>
-        <Route path="/signup" element={!user ? <SignUpPage/> : <Navigate to={"/"}/>}/>       
+        <Route path="/signup" element={!user ? <SignUpPage/> : <Navigate to={"/"}/>}/> 
+        <Route path="/watch/:id" element={user ? <WatchPage/> : <Navigate to={"/login"}/>}/>       
+      
       </Routes>
       <Footer/>
 
